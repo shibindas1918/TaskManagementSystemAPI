bc41802a55d48425aa605b276dc6ab1c79902074
sibin <sibindas1806@gmail.com>
2024-12-12 16:44:46 +0530

    Added connection string , migrations were created

--- TaskManagementSystemAPI/Program.cs
+++ TaskManagementSystemAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagementSystemAPI.Data;
+using TaskManagementSystemAPI.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +10,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddScoped<TaskService>();
+builder.Services.AddDbContext<TaskManagementContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+
+
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
